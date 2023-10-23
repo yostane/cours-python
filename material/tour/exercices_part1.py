030c01984c0354ad200b5afa3b10938145923f78
yostane <1958676+yostane@users.noreply.github.com>
2023-10-23 11:58:59 +0200

    intersect 2d

--- material/tour/exercices_part1.py
+++ material/tour/exercices_part1.py
@@ -58,7 +58,7 @@ print(count_letters3("hello"))
 def is_intersect_1D(x1, l1, x2, l2):
     # Dans d'autres langages, il aurait fallu écrire (On ne peut pas combiner les inégalités)
     # x1 < x2 && x2 < x1 + l1 || x2 < x1 && x1 < x2 + l2
-    return x1 < x2 < x1 + l1 or x2 < x1 < x2 + l2
+    return x1 <= x2 <= x1 + l1 or x2 <= x1 <= x2 + l2
 
 
 print(is_intersect_1D(0, 10, 3, 1))
@@ -71,14 +71,17 @@ print(is_intersect_1D(100, 1, -5, 99))
 
 
 def is_intersect_2D(rec1, rec2):
-    pass
+    return is_intersect_1D(
+        rec1["x"], rec1["width"], rec2["x"], rec2["width"]
+    ) and is_intersect_1D(rec1["y"], rec1["height"], rec2["y"], rec2["height"])
 
 
 rec1 = {"x": 10, "y": 30, "width": 100, "height": 200}
 rec2 = {"x": -5, "y": 20, "width": 50, "height": 100}
-rec3 = {"x": -5, "y": 20, "width": 50, "height": 100}
-rec4 = {"x": -5, "y": 20, "width": 50, "height": 100}
-print(is_intersect_2D(rec1, rec2))
-print(is_intersect_2D(rec1, rec3))
-print(is_intersect_2D(rec1, rec4))
-print(is_intersect_2D(rec2, rec3))
+rec3 = {"x": 15, "y": 15, "width": 5, "height": 5}
+rec4 = {"x": 15, "y": 15, "width": 5, "height": 400}
+print("testing intersect 2D")
+print("rec1, rec2", is_intersect_2D(rec1, rec2))
+print("rec1, rec3", is_intersect_2D(rec1, rec3))
+print("rec1, rec4", is_intersect_2D(rec1, rec4))
+print("rec2, rec3", is_intersect_2D(rec2, rec3))
