7f1bdb7b51f365e500bf9a166b5470861672c61b
yostane <1958676+yostane@users.noreply.github.com>
2023-10-23 12:48:02 +0200

    Correction exo plot

--- material/tour/exercices_part1_plot.py
+++ material/tour/exercices_part1_plot.py
@@ -2,16 +2,34 @@ import matplotlib.pyplot as plt
 
 
 def fx_square(x):
-    pass
+    return x * x
 
 
 def fx_square_list(n):
-    pass
+    items = []
+    for i in range(n):
+        items.append(i * i)
+    return items
 
 
 def plot_f(n):
-    pass
+    plt.plot(range(n), fx_square_list(n))
+    plt.show()
 
 
-fx_square_list(2, 10)
-plot_f(2, 10)
+n = 88
+print(fx_square_list(n))
+plot_f(n)
+
+
+def fx_square_list2(points):
+    values = []
+    for point in points:
+        values.append(point * point)
+    return values
+
+
+points = range(-100, 100)
+values = fx_square_list2(points)
+plt.plot(points, values)
+plt.show()
