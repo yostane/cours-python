eb8f92767f88e7c5187d5d3f8490713691017d13
yostane <1958676+yostane@users.noreply.github.com>
2024-09-16 17:18:34 +0200

    Add lecture exercises for positive/negative integer, ascending order, and even/odd number

--- material/2024-2025/lecture_exercises.py
+++ material/2024-2025/lecture_exercises.py
@@ -1,9 +1,16 @@
-""" Lecture Ex 01
+""" Lecture Ex 1
 Write a Python script that asks the user to enter an integer.
 Then, it tells the user if the integer is positive or negative
 """
 
-""" Lecture Ex 02
+""" Lecture Ex 2
 Write a Python script that asks the user to enter two integers.
 Then, it shows them in the ascending order.
+"""
+
+""" Lecture Ex 3
+Write a Python script that asks the user to enter an integer.
+Then, it prints 
+"even" if the number is even (pair en Français), or
+"odd" if the number is off (impair en Français).
 """
