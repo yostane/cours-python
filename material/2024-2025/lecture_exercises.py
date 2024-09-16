76db5d8ef0c67940d1f82e56a7b5725cb618687c
yostane <1958676+yostane@users.noreply.github.com>
2024-09-16 17:03:11 +0200

    Add lecture exercises for positive/negative integer and ascending order

---
+++ material/2024-2025/lecture_exercises.py
@@ -0,0 +1,9 @@
+""" Lecture Ex 01
+Write a Python script that asks the user to enter an integer.
+Then, it tells the user if the integer is positive or negative
+"""
+
+""" Lecture Ex 02
+Write a Python script that asks the user to enter two integers.
+Then, it shows them in the ascending order.
+"""
