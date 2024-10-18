4414f3c2f965f698c157bcf58f9d427c77d80307
yostane <1958676+yostane@users.noreply.github.com>
2024-10-18 09:29:25 +0200

    Add functions for multiplication, finding the biggest value, and finding the smallest value

---
+++ material/2024-2025/CPI/function_exercices_01.py
@@ -0,0 +1,19 @@
+"""
+Ex1: Define a function that takes two arguments and returns their multiplication.
+Call this function 4 times and print the results.
+"""
+
+"""
+Ex2: Define a function that takes two integer arguments and returns the biggest value.
+Call this function 4 times and print the results.
+"""
+
+"""
+Ex3: Define a function that takes three integer arguments  and returns the smallest value.
+Call this function 4 times and print the results.
+"""
+
+"""
+Ex3: Define a function that takes three integer arguments  and returns the smallest value.
+Call this function 4 times and print the results.
+"""
