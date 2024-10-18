787f4bfbe596ad60a694c658b1a87f0b892ae7c6
yostane <1958676+yostane@users.noreply.github.com>
2024-10-18 11:46:15 +0200

    Refactor code to improve readability and add function for printing the size of a list

--- material/2024-2025/CPI/lists.py
+++ material/2024-2025/CPI/lists.py
@@ -16,3 +16,19 @@ print("Third element of items or element at index 2", items[2])
 print("Last element of items", items[-1], items[3])
 # Indexing can go backwards with -
 print(items[-1], items[-2], items[-3], items[-4])
+
+# indexing [] also works with strings to get signle caracters
+message = "Python is the best language"
+print(message[0], message[9], message[-2])
+
+# len is a predefined function that returns the number of items
+print("len(items)", len(items), "len(message)", len(message))
+
+
+# A list can be passed to a funtion
+def print_size(items):
+    print(len(items))
+
+
+print_size(items)
+print_size([1, 9, 100])
