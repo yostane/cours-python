5f05a9be0cd80b4d1ac6c7ef4bc037e4ee9ccd3e
yostane <1958676+yostane@users.noreply.github.com>
2024-10-18 09:26:15 +0200

    Refactor code to improve readability and add functions for checking if a number is even and adding two numbers

--- material/2024-2025/CPI/demo02.py
+++ material/2024-2025/CPI/demo02.py
@@ -12,14 +12,31 @@ if a % 2 == 0:
 
 
 # A function avoids repeating the same code
-def is_even():
-    if a % 2 == 0:
+# Function definition with an argument
+def is_even(x):
+    if x % 2 == 0:
         return True
     else:
         return False
 
 
-if is_even():
+# Function call
+if is_even(a):
     print("a is even")
 else:
     print("a is odd")
+
+d = 21
+if is_even(d):
+    print("a is even")
+else:
+    print("a is odd")
+
+
+# A function with two arguments
+def add(x, y):
+    return x + y
+
+
+sum1 = add(4, 9)
+print(sum1, add(-8, 0))
