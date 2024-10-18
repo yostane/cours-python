dc252862df552ab4eea175afb8e9d0693c1903f8
yostane <1958676+yostane@users.noreply.github.com>
2024-10-18 10:11:40 +0200

    Refactor code to improve readability and add function for multiplying two numbers

--- material/2024-2025/CPI/function_exercices_01.py
+++ material/2024-2025/CPI/function_exercices_01.py
@@ -3,6 +3,17 @@ Ex1: Define a function that takes two arguments and returns their multiplication
 Call this function 4 times and print the results.
 """
 
+
+def multiply(x, y):
+    return x * y
+
+
+a1 = multiply(2, 4)
+a2 = multiply(1, 0)
+a3 = multiply(1, -7)
+a4 = multiply(10, 99)
+print(a1, a2, a3, a4)
+
 """
 Ex2: Define a function that takes two integer arguments and returns the biggest value.
 Call this function 4 times and print the results.
