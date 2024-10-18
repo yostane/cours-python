39fe4a865b117a3a88ea89f318355ab677b74b08
yostane <1958676+yostane@users.noreply.github.com>
2024-10-18 11:50:27 +0200

    Refactor code to improve readability and add functions for printing the values of the first and last elements of a list, checking the length of a string, and identifying the third character of a string.

--- material/2024-2025/CPI/lists_exercise_01.py
+++ material/2024-2025/CPI/lists_exercise_01.py
@@ -1,1 +1,17 @@
-"""write a function that takes a list"""
+"""Exercise 1:
+write a function that takes a list argument and prints the values of the first and last elemets
+call this function 2 times
+"""
+
+"""Exercise 2:
+write a function that takes a string and an int argument. It prints:
+- Long: If the length of the string is greater than the int argument
+- Short: otherwise
+"""
+
+"""Exercise 3:
+Write a function that takes a string argument. This function prints
+- space: if the third caracter is a space
+- other: otherwise
+- error: if the argument has less than three characters
+"""
