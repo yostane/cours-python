c97ef0f5e7d8a9544267a84ebed1947ec33fda72
yostane <1958676+yostane@users.noreply.github.com>
2024-10-18 10:23:10 +0200

    Refactor code to improve readability and add function for multiplying two numbers

--- material/2024-2025/CPI/function_exercices_01.py
+++ material/2024-2025/CPI/function_exercices_01.py
@@ -1,10 +1,8 @@
-"""
-Ex1: Define a function that takes two arguments and returns their multiplication.
-Call this function 4 times and print the results.
-"""
-
-
 def multiply(x, y):
+    """
+    Ex1: Define a function that takes two arguments and returns their multiplication.
+    Call this function 4 times and print the results.
+    """
     return x * y
 
 
@@ -29,4 +27,12 @@ Ex4: Define a function that takes three arguments: an int and two strings.
 if the first argument is even, the function returns the first string argument.
 Otherwise, it returns the second string argument.
 Call this function 4 times and print the results.
+"""
+
+"""
+Ex5: Define a function that takes an int argument. Implement the function as follows.
+The function reads an interger from the keyboard (with input) and prints:
+- "You win" if the entered number is bigger than the argument
+- "You lose" otherwise
+Call this function 1 time.
 """
