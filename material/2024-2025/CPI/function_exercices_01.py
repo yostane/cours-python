524e122744f4d5c15f2f40472a3865f5e179034e
yostane <1958676+yostane@users.noreply.github.com>
2024-10-18 11:11:58 +0200

    Refactor code to improve readability and add functions for multiplication, finding the biggest value, and finding the smallest value

--- material/2024-2025/CPI/function_exercices_01.py
+++ material/2024-2025/CPI/function_exercices_01.py
@@ -6,6 +6,7 @@ def multiply(x, y):
     return x * y
 
 
+print("Exercice 1")
 a1 = multiply(2, 4)
 a2 = multiply(1, 0)
 a3 = multiply(1, -7)
@@ -28,13 +29,30 @@ def get_max2(x, y):
     return x if x >= y else y
 
 
+print("Exercice 2")
 print(get_max(3, -8), get_max(0, 1), get_max2(1000, 100), get_max2(-7987, 333))
 
 
-"""
-Ex3: Define a function that takes three integer arguments and returns the smallest value.
-Call this function 4 times and print the results.
-"""
+def get_min_three(a, b, c):
+    """
+    Ex3: Define a function that takes three integer arguments and returns the smallest value.
+    Call this function 4 times and print the results.
+    """
+    if a <= b and a <= c:
+        return a
+    elif b <= a and b <= c:
+        return b
+    else:
+        return c
+
+
+print("Exercice 3")
+print(
+    get_min_three(0, 1, 2),
+    get_min_three(10, -3, 99),
+    get_min_three(85, 12, 21),
+    get_min_three(7, 11, -2),
+)
 
 """
 Ex4: Define a function that takes three arguments: an int and two strings.
@@ -45,14 +63,6 @@ Call this function 4 times and print the results.
 
 """
 Ex5: Define a function that takes an int argument. Implement the function as follows.
-The function reads an interger from the keyboard (with input) and prints:
-- "You win" if the entered number is bigger than the argument
-- "You lose" otherwise
-Call this function 1 time.
-"""
-
-"""
-Ex6: Define a function that takes an int argument. Implement the function as follows.
 The function reads an interger from the keyboard (with input) and prints:
 - "You win" if the entered number is bigger than the argument
 - "You lose" otherwise
