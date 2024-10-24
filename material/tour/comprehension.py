dc22950b4601fdce79e28ff9f9191c51301809b2
yostane <1958676+yostane@users.noreply.github.com>
2024-10-24 15:14:23 +0200

    Refactor code to improve readability and add functions for printing the values of the first and last elements of a list, checking the length of a string, and identifying the third character of a string
    - Double each element in a list using comprehensions
    - Calculate the fourth power of integers between -2 and 9 with a step of 2
    - Filter out only the even elements from a list
    - Calculate the length of each string in a list and add a period at the end
    - Create a dictionary with strings as keys and their lengths as values
    - Create a set with the lengths of the strings
    - Generate a card game deck using ranks and symbols
    - Transform a matrix into a list using comprehensions
    - Generate a list of strings representing the elements of a matrix with their indices
    - Refactor the code to use nested for loops instead of comprehensions for the last two examples

--- material/tour/comprehension.py
+++ material/tour/comprehension.py
@@ -1,15 +1,24 @@
-# Double de chaque élément
 items = [0, 1, 2, 3, 4]
+
+# Double de chaque élément
+double_items_1 = []
+for item in items:
+    double_items_1.append(item * 2)
+print("double_items_1", double_items_1)
+
+# Transformer (to map en Anglais) avec les compréhensions
 results = [item * 2 for item in items]
 print(results)
 print([item * 2 for item in items])
-# puissance 4
+# puissance 4 des entier entre -2 et 9 avec un pas de 2
 print([item**4 for item in range(-2, 10, 2)])
 
+# Filtrage
 # Garder uniquement les éléments pairs
 items = [9, 22, -2, 11, 1232, 2323]
 print([item for item in items if item % 2 == 0])
 
+# Equivalent avec un boucle for
 results = []
 for item in items:
     if item % 2 == 0:
@@ -21,6 +30,26 @@ print([item * 3 for item in items if item % 2 == 0])
 
 messages = ["Hello", "J'aime", "Python"]
 print([len(message) for message in messages])
-print([message + "." for message in messages])
+print([f"{message}." for message in messages])
+# Dict (dictionnaire)
 print({message: len(message) for message in messages})
+# Set ensemble
 print({len(message) for message in messages})
+
+# Card game from ranks and symbols
+ranks = range(1, 11)
+symbols = ["💗", "♠️", "🍀", "🔶"]
+# Produit cartésien
+print([f"{r}{s}" for r in ranks for s in symbols])
+
+m = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+print([item for row in m for item in row])
+row_count = range(len(m))
+print([f"m[{i}, {j}] => {m[i][j]}" for i in row_count for j in range(len(m[i]))])
+
+# Equivalent en for
+result = []
+for i in row_count:
+    for j in range(len(m[i])):
+        result.append(f"m[{i}, {j}] => {m[i][j]}")
+print("Tranformation matrice vers liste avec for", result)
