544b353696488bfc514ff9833ceac834e31001ba
yostane <1958676+yostane@users.noreply.github.com>
2024-11-22 11:30:36 +0100

    Add new Tkinter examples with labels and button interactions

---
+++ material/2024-2025/CPI/exercises/08-tkinter-01.py
@@ -0,0 +1,16 @@
+"""Create a tkinter app that shows an a label and a button
+The label shows 0 initially and is incremented after each click on the button"""
+
+"""Create a tkinter app that shows two labels and a button
+The labels show 0 initially. When we click on the button the first time, the first label
+increments, when we click on the button a second time, the second label increments.
+When we click on the button a third time, the first label increments and so on"""
+
+"""Create a tinkter app that shows a button on top and a label below it.
+When we click on the button, the labal adds a new line with the text "line {i}".
+For example, if we click on the button 4 times, then the label shows:
+line 1
+line 2
+line 3
+line 4
+"""
--- material/2024-2025/CPI/lecture/08-tkinter-01.py
+++ material/2024-2025/CPI/lecture/08-tkinter-01.py
@@ -5,11 +5,13 @@ from tkinter import ttk
 window = tk.Tk()
 window.geometry("600x300")
 
-def print_message():
-    print("button pressed")
 
 label = ttk.Label(window, text="Hello World")
 label2 = ttk.Label(window, text="Other text")
+
+def print_message():
+    label.config(text="The button has been pressed")
+    print("button pressed")
 button = ttk.Button(window, text="Click Me", command=print_message)
 
 # Change the order of pack and see what happens
