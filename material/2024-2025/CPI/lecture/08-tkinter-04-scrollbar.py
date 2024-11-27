49b9b6b28a76e92e100173f3e178add0afa42e82
yostane <1958676+yostane@users.noreply.github.com>
2024-11-27 10:37:53 +0100

    Vertical scrolled frame

---
+++ material/2024-2025/CPI/lecture/08-tkinter-04-scrollbar.py
@@ -0,0 +1,18 @@
+""" Vertical scroll example. The file VerticalScrolledFrame.py must be copied into your project  """
+import tkinter as tk
+from tkinter import ttk
+from VerticalScrolledFrame import VerticalScrolledFrame
+
+window = tk.Tk()
+window.title('VerticalScrolledFrame demo')
+window.geometry('400x300')
+window.config(bg='#5F734C')
+
+frame = VerticalScrolledFrame(window)
+
+frame.pack(expand = True, fill = tk.BOTH)
+
+for i in range(10):
+    ttk.Button(frame.interior, text=f"Button {i}").pack(padx=10, pady=5)
+
+window.mainloop()
